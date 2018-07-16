0ff38190d86dc95a5041f430b42d14004d466653
aeroanshul <anshul.arunachalam@gmail.com>
2018-07-16 12:56:35 -0700

    bug fixes and added more options

--- data_avg.py
+++ data_avg.py
@@ -1,4 +1,5 @@
 #code to average data and graph data
+#to install dependencies: conda install selenium phantomjs pillow
 
 import csv
 import urllib.request
@@ -8,6 +9,7 @@ import datetime
 
 from bokeh.plotting import figure, show, output_file, save
 from bokeh.models import LinearAxis, Range1d
+from bokeh.io import export_svgs
 
 class data_average:
     def _init_(self):
@@ -36,24 +38,23 @@ class data_average:
             index_list= list(np.where(np.logical_and(time_data >= float(early) , time_data < float(late))))
             counter = index_list[0].size
             if counter == 0:
-                early = late
-                late += avg_over_time
-                bin_number -= 1
-            upper_bound = index_list[-1]
-            to_be_avged = []
-            errors = []
-            while counter > 0:
-                b1 = csv_data[upper_bound - counter - 1, column-1]
-                b = [float(n) for n in b1]
-                c1 = csv_data[upper_bound - counter - 1, -1]
-                c = [float(n) for n in c1]
-                to_be_avged.append(b)
-                errors.append(c)
-                counter -= 1
-            avg_time = (early+late)/2
-            bin_avg = np.average(to_be_avged)
-            error_avg = np.average(errors)
-            avgd_data_points.append([avg_time,bin_avg,error_avg])
+                pass
+            else:
+                upper_bound = index_list[-1]
+                to_be_avged = []
+                errors = []
+                while counter > 0:
+                    b1 = csv_data[upper_bound - counter - 1, column-1]
+                    b = [float(n) for n in b1]
+                    c1 = csv_data[upper_bound - counter - 1, -1]
+                    c = [float(n) for n in c1]
+                    to_be_avged.append(b)
+                    errors.append(c)
+                    counter -= 1
+                avg_time = (early+late)/2
+                bin_avg = np.average(to_be_avged)
+                error_avg = np.average(errors)
+                avgd_data_points.append([avg_time,bin_avg,error_avg])
             early = late
             late += avg_over_time
             print(bin_number)
@@ -64,40 +65,123 @@ class avg_graph:
     def _init_(self):
         pass
 
-    def graph(self, points, error, to_do):
-        plot = figure(plot_width = 1000, plot_height = 1000, tools="pan,wheel_zoom,box_zoom,reset")
-        i = len(points)
+    def graph(self, points, points2, error, to_do, file_type, graph_type):
+        if len(points) == 0:
+            print('Error - Data1 empty')
+            return
+        if points2 != False:
+            if len(points2) == 0:
+                print('Error - Data2 empty')
+                return
+        if graph_type == 2:
+            plot = figure(plot_width = 1000, plot_height = 1000, tools="pan,wheel_zoom,box_zoom,reset", x_axis_type='datetime')
+            pointsdata = [i[1] for i in points]
+            min_val = 0.25*min(pointsdata)
+            max_val = 1.75*max(pointsdata)
+            plot.y_range=Range1d(min_val, max_val)
+            i = len(points)
+            if error == 'Y':
+                while i > 0:
+                    print(i)
+                    a = datetime.datetime.fromtimestamp(points[i-1][0])
+                    plot.vbar(x=a, top=points[i-1][1]+points[i-1][2], bottom=points[i-1][1]-points[i-1][2], width = 0.01, color = "red")
+                    i -= 1
 
-        while i > 0:
-            print(i)
-            plot.circle(x=points[i-1][0], y=points[i-1][1])
-            i -= 1
-
-        i = len(points)
-        if error == 'Y':
+            i = len(points)
             while i > 0:
                 print(i)
-                plot.vbar(x=points[i-1][0],top=points[i-1][2], bottom = 0, width = 1, color = "red")
+                a = datetime.datetime.fromtimestamp(points[i-1][0])
+                plot.circle(x=a, y=points[i-1][1])
+                i -= 1
+
+            if points2 != False:
+                points2data = [i[1] for i in points2]
+                min_val2 = 0.25*min(points2data)
+                max_val2 = 1.75*max(points2data)
+                plot.extra_y_ranges = {'data2': Range1d(start = min_val2, end=max_val2)}
+                plot.add_layout(LinearAxis(y_range_name='data2'), 'right')
+                i = len(points2)
+                if error == 'Y':
+                    while i > 0:
+                        print(i)
+                        a = datetime.datetime.fromtimestamp(points2[i-1][0])
+                        plot.vbar(x=a, top=points2[i-1][1]+points2[i-1][2], bottom=points2[i-1][1]-points2[i-1][2], width = 0.01, color = "green", y_range_name='data2')
+                        i -= 1
+
+                i = len(points2)
+                while i > 0:
+                    print(i)
+                    a = datetime.datetime.fromtimestamp(points2[i-1][0])
+                    plot.triangle(x=a, y=points2[i-1][1], y_range_name='data2', color = 'yellow')
+                    i -= 1
+
+        else:
+            plot = figure(plot_width = 1000, plot_height = 1000, tools="pan,wheel_zoom,box_zoom,reset")
+            i = len(points2)
+            if error == 'Y':
+                while i > 0:
+                    print(i)
+                    plot.vbar(x=points[i-1][1], top=points2[i-1][1]+points2[i-1][2], bottom=points2[i-1][1]-points2[i-1][2], width = 0.01, color = "red")
+                    plot.hbar(y=points2[i-1][1], left=points[i-1][1]+points[i-1][2], right=points[i-1][1]-points[i-1][2], height = 0.01, color = "green")
+                    i -= 1
+
+            i = len(points)
+            while i > 0:
+                try:
+                    print(i)
+                    plot.circle(x=points[i-1][1], y=points2[i-1][1], radius = 0.01)
+                except:
+                    pass
                 i -= 1
 
         if to_do == 'Y':
             show(plot)
         else:
-            output_file(to_do+'.html')
+            if file_type == 'html':
+                output_file(to_do+'.html')
+            else:
+                a = to_do+".svg"
+                plot.output_backend = "svg"
+                export_svgs(plot, filename=a)
             save(plot)
 
 a = str(input('csv url: '))
+a2 = str(input('csv2 (url/N): '))
 b = int(input('start time (sec since epoch): '))
 c = int(input('stop time: '))
 d = int(input('# of sec to avg over: '))
 e = int(input('column of csv to avg: '))
+'''
+a = 'https://radwatch.berkeley.edu/sites/default/files/dosenet/etch_roof.csv'
+a2 = 'https://radwatch.berkeley.edu/sites/default/files/dosenet/etch_roof_adc.csv'
+b = 1530859550
+c = 1531166451
+d = 10000
+e = 4
+e2 = 4
+'''
+if a2 != 'N':
+    e2 = int(input('column of csv2 to avg: '))
+    g = int(input('1) data1 v d2 OR 2) d1,d2 v time (1/2): '))
+else:
+    g = 2
 f = str(input('plot with error? (Y/N): '))
-g = str(input('save or show (save: file name, show: Y): '))
+h = str(input('save or show graph (save: file name, show: Y): '))
+if h != 'Y':
+    j = str(input('output file format? (svg/html): '))
+else:
+    j = False
 
 print('Progress:')
 avger = data_average()
 final_data_list = avger.avg_main(a, b, c, d, e)
 
+
+if a2 != 'N':
+    final_data_list2 = avger.avg_main(a2, b, c, d, e2)
+else:
+    final_data_list2 = False
+
 grapher = avg_graph()
-grapher.graph(final_data_list, f, g)
+grapher.graph(final_data_list, final_data_list2, f, h, j, g)
 print('Finished')
